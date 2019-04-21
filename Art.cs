907ab050ea174ce5530bada7a9f7202ca4fa52c2
aopheim <37472517+aopheim@users.noreply.github.com>
2019-04-21 18:46:28 +0200

    Created data-driven class Art

---
+++ Art.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestefarsBilder
+{
+    public class Art
+    {
+        public int id;
+        public string title;
+        public string artform;
+        public string exhibition;
+        public string dimensions;
+        public string year;
+        public string comment;
+
+        public Art(int id)
+        {
+            this.id = id;
+        }
+
+        public Art(int id, string title, string artform, 
+            string exhibition, string dimensions, string year, string comment)
+        {
+            this.id = id;
+            this.title = title;
+            this.artform = artform;
+            this.exhibition = exhibition;
+            this.dimensions = dimensions;
+            this.year = year;
+            this.comment = comment;
+        }
+    }
+}
