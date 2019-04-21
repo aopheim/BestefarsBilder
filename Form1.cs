e2318e3d682d5a1ef51ad3edcbebf1f9d0920ffe
aopheim <37472517+aopheim@users.noreply.github.com>
2019-04-21 18:47:07 +0200

    Successfully created JSON object

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -65,6 +66,21 @@ namespace BestefarsBilder
         private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int id = Int32.Parse(intID.Text);
+            string title = strTitle.Text;
+            string artForm = cmbxArtForm.Text;
+            string exhibition = cmbxExhibition.Text;
+            string dimensions = cmbxDimensions.Text;
+            String year = txtbxYear.Text;       // Saving as string makes it possible to save no year as ""
+            string comment = txtbxComment.Text;
+
+            Art art = new Art(id, title, artForm, exhibition, dimensions, year, comment);
+            string jsonObj = JsonConvert.SerializeObject(art, Formatting.Indented);
+            Console.WriteLine(jsonObj);
         }
     }
 }
