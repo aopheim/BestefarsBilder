f333113ab7f5cc43e153987a7d43e42d363f0846
aopheim <37472517+aopheim@users.noreply.github.com>
2019-05-17 21:34:33 +0200

    Added Graphics class

---
+++ Graphics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BestefarsBilder
+{
+    class Graphics
+    {
+        private Color inactiveColor = SystemColors.InactiveCaption;
+        private Color activeColor = SystemColors.Window;
+        private Color warningColor = Color.Red;
+        private List<TextBox> txtBoxes;
+        private List<ComboBox> comboBoxes;
+
+
+        public Graphics()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
