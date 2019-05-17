dd3ab93583466bf1dde106749a2e43795f084e91
aopheim <37472517+aopheim@users.noreply.github.com>
2019-05-17 22:22:51 +0200

    Create GraphicsTests.cs

---
+++ UnitTest/GraphicsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BestefarsBilder.Test
+
+{
+    [TestClass]
+    public class GraphicsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
