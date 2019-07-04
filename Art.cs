7bff1c6f17e0c7659debe93ef12083a1f067ed07
aopheim <37472517+aopheim@users.noreply.github.com>
2019-07-04 23:57:34 +0200

    Added tags, placement, lastEdit data field

--- Art.cs
+++ Art.cs
@@ -15,8 +15,10 @@ namespace BestefarsBilder
         public string dimensions { get; set; }
         public string year { get; set; }
         public string comment { get; set; }
+        public string tags { get; set; }
+        public string placement { get; set; }
         public int numImageFiles { get; set; }
-
+        public DateTime lastEdit = DateTime.Now;
 
 
         public override string ToString() {
@@ -27,8 +29,10 @@ namespace BestefarsBilder
                 "Utstilling: " + exhibition + "\n" +
                 "Dimensjoner: " + dimensions + "\n" +
                 "År: " + year + "\n" +
+                "Sist endret: " + lastEdit.ToString("dd-MM-yyyy HH:mm:ss") + "\n" +
+                "Plassering: " + placement + "\n" +
+                "Tags: " + tags + "\n" +
                 "Kommentar: " + comment + "\n";
         }
-
     }
 }
